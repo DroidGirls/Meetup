d928f57f7cb5201c9c67b76f2f7bd4f440ac2a4c
konifar <yahpeycoy0403@gmail.com>
2017-01-25 07:06:15 +0900

    サンプルプロジェクトを追加

---
+++ 3_vector_drawable/example/app/src/main/java/io/droidgirls/vectordrawable/training/MainActivity.java
@@ -0,0 +1,14 @@
+package io.droidgirls.vectordrawable.training;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
